c83351d3f902101c4a1faffe6091f13c4779ce90
jbbastos1303 <jbbastos.dev@gmail.com>
2026-01-14 20:59:36 -0300

    imagens em jpg

--- src/pages/galeria.js
+++ src/pages/galeria.js
@@ -6,38 +6,38 @@ import styles from "./galeria.module.css";
 
 /* frota */
 const ITEMS = [
-  { id: 1, src: "/images/frota1.png", thumb: "/images/frota1.png", type: "image", tag: "frota", alt: "Escavadeira em operação 1" },
-  { id: 2, src: "/images/frota2.png", thumb: "/images/frota2.png", type: "image", tag: "frota", alt: "Escavadeira em operação 2" },
-  { id: 3, src: "/images/frota3.png", thumb: "/images/frota3.png", type: "image", tag: "frota", alt: "Escavadeira em operação 3" },
-  { id: 4, src: "/images/frota4.png", thumb: "/images/frota4.png", type: "image", tag: "frota", alt: "Escavadeira em operação 4" },
-  { id: 5, src: "/images/frota5.png", thumb: "/images/frota5.png", type: "image", tag: "frota", alt: "Escavadeira em operação 5" },
-  { id: 6, src: "/images/frota6.png", thumb: "/images/frota6.png", type: "image", tag: "frota", alt: "Escavadeira em operação 6" },
-  { id: 7, src: "/images/frota7.png", thumb: "/images/frota7.png", type: "image", tag: "frota", alt: "Escavadeira em operação 7" },
-  { id: 8, src: "/images/frota8.png", thumb: "/images/frota8.png", type: "image", tag: "frota", alt: "Escavadeira em operação 8" },
+  { id: 1, src: "/images/frota1.jpg", thumb: "/images/frota1.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 1" },
+  { id: 2, src: "/images/frota2.jpg", thumb: "/images/frota2.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 2" },
+  { id: 3, src: "/images/frota3.jpg", thumb: "/images/frota3.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 3" },
+  { id: 4, src: "/images/frota4.jpg", thumb: "/images/frota4.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 4" },
+  { id: 5, src: "/images/frota5.jpg", thumb: "/images/frota5.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 5" },
+  { id: 6, src: "/images/frota6.jpg", thumb: "/images/frota6.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 6" },
+  { id: 7, src: "/images/frota7.jpg", thumb: "/images/frota7.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 7" },
+  { id: 8, src: "/images/frota8.jpg", thumb: "/images/frota8.jpg", type: "image", tag: "frota", alt: "Escavadeira em operação 8" },
 
 
   // exemplos para outras seções
-  { id: 9, src: "/images/obra1.png", thumb: "/images/obra1.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 10, src: "/images/obra2.png", thumb: "/images/obra2.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 11, src: "/images/obra3.png", thumb: "/images/obra3.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 12, src: "/images/obra4.png", thumb: "/images/obra4.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 13, src: "/images/obra5.png", thumb: "/images/obra5.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 14, src: "/images/obra6.png", thumb: "/images/obra6.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 15, src: "/images/obra7.png", thumb: "/images/obra7.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 16, src: "/images/obra8.png", thumb: "/images/obra8.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 17, src: "/images/obra9.png", thumb: "/images/obra9.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 18, src: "/images/obra10.png", thumb: "/images/obra10.png", type: "image", tag: "obra", alt: "Terraplenagem" },
-  { id: 19, src: "/images/obra11.png", thumb: "/images/obra11.png", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 9, src: "/images/obra1.jpg", thumb: "/images/obra1.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 10, src: "/images/obra2.jpg", thumb: "/images/obra2.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 11, src: "/images/obra3.jpg", thumb: "/images/obra3.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 12, src: "/images/obra4.jpg", thumb: "/images/obra4.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 13, src: "/images/obra5.jpg", thumb: "/images/obra5.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 14, src: "/images/obra6.jpg", thumb: "/images/obra6.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 15, src: "/images/obra7.jpg", thumb: "/images/obra7.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 16, src: "/images/obra8.jpg", thumb: "/images/obra8.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 17, src: "/images/obra9.jpg", thumb: "/images/obra9.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 18, src: "/images/obra10.jpg", thumb: "/images/obra10.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
+  { id: 19, src: "/images/obra11.jpg", thumb: "/images/obra11.jpg", type: "image", tag: "obra", alt: "Terraplenagem" },
 
 
-  { id: 20, src: "/images/equipe1.png", thumb: "/images/equipe1.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
-  { id: 21, src: "/images/equipe2.png", thumb: "/images/equipe2.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
-  { id: 22, src: "/images/equipe3.png", thumb: "/images/equipe3.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
-  { id: 23, src: "/images/equipe4.png", thumb: "/images/equipe4.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
-  { id: 24, src: "/images/equipe5.png", thumb: "/images/equipe5.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
-  { id: 25, src: "/images/equipe6.png", thumb: "/images/equipe6.png", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 20, src: "/images/equipe1.jpg", thumb: "/images/equipe1.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 21, src: "/images/equipe2.jpg", thumb: "/images/equipe2.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 22, src: "/images/equipe3.jpg", thumb: "/images/equipe3.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 23, src: "/images/equipe4.jpg", thumb: "/images/equipe4.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 24, src: "/images/equipe5.jpg", thumb: "/images/equipe5.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
+  { id: 25, src: "/images/equipe6.jpg", thumb: "/images/equipe6.jpg", type: "image", tag: "equipe", alt: "Equipe em campo" },
 
-  { id: 26, src: "/images/video-obra.mp4", thumb: "/images/video-obra.jpg", type: "video", tag: "video", alt: "Vídeo institucional" }
+  { id: 26, src: "/images/video-book.mp4", thumb: "/images/video-book.jpg", type: "video", tag: "video", alt: "Vídeo institucional" }
 ];
 
 const FILTERS = [
